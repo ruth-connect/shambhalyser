58c1e857170eb9a59650b916334368f66975be42
ruth <ruth@DESKTOP-HOVL7PT>
2022-09-25 11:21:48 +0100

    Initial commit of Spring Boot stuff.

---
+++ src/main/java/uk/me/ruthmills/shambhalyser/ShambhalyserApplication.java
@@ -0,0 +1,13 @@
+package uk.me.ruthmills.shambhalyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShambhalyserApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShambhalyserApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/me/ruthmills/shambhalyser/ShambhalyserApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.me.ruthmills.shambhalyser;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShambhalyserApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
